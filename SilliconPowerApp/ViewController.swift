56d936ec7712a5a95300d0742172f9e60a067e33
Bitos88 <tyrion72@gmail.com>
2022-02-24 14:07:27 +0100

    Initial Commit

---
+++ SilliconPowerApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SilliconPowerApp
+//
+//  Created by Alberto Alegre Bravo on 24/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
